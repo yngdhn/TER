b0b9bd6788ad5338e5a4ee4e9270b6176d1a0e3b
yngdhn <166201088+yngdhn@users.noreply.github.com>
2024-12-03 14:49:21 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,14 +1,16 @@
 class Paths:
     TRAIN_DATA = "dataset/train_data.csv"
     TEST_DATA = "dataset/test_data.csv"
-    SINGLE_ROBERTA = "models/single_transfer_RoBERTa-pt"
-    DOUBLE_BERT = "models/double_transfer_BERT. pt"
+
+    SINGLE_ROBERTA = "models/single_transfer_RoBERTa.pt"
+    DOUBLE_BERT = "models/double_transfer_BERT.pt"
 
 class SingleTransferRobertaConfig:
     MODEL_NAME = "roberta-base"
     NUM_LABELS = 5
     DROPOUT = 0.1
     MAX_LENGTH = 128
+
     EMOTIONS = {"Neutral": 0, "Angry": 1, "Happy": 2, "Sad": 3, "Surprise": 4}
 
 class DoubleTransferBertConfig:
@@ -16,4 +18,5 @@ class DoubleTransferBertConfig:
     NUM_LABELS = 5
     DROPOUT = 0.1
     MAX_LENGTH = 128
+    
     EMOTIONS = {"Neutral": 0, "Angry": 1, "Happy": 2, "Sad": 3, "Surprise": 4}
