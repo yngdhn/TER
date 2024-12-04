c114019603dfbc4e9c41bb7d46a6c131b778aeff
yngdhn <166201088+yngdhn@users.noreply.github.com>
2024-12-04 20:34:06 +0900

    Update config.py - add model

--- src/config.py
+++ src/config.py
@@ -5,10 +5,27 @@ class Paths:
     TEST_DATA = "dataset/test_data.csv"
 
     SINGLE_ROBERTA = "models/single_transfer_RoBERTa.pt"
+    SINGLE_BERT = "models/single_transfer_BERT.pt"
     DOUBLE_BERT = "models/double_transfer_BERT.pt"
 
 class SingleTransferRoBERTaConfig:
     MODEL_NAME = "roberta-base"
+    NUM_LABELS = 6
+    DROPOUT = 0.1
+    MAX_LENGTH = 512
+
+    EPOCHS = 10
+    BATCH_SIZE = 64
+
+    LEARNING_RATE = 2e-5
+    WEIGHT_DECAY = 0.01
+
+    PATIENCE = 3
+
+    EMOTIONS = {"sadness": 0, "joy": 1, "love": 2, "anger": 3, "fear": 4, "surprise": 5}
+
+class SingleTransferBERTConfig:
+    MODEL_NAME = ""
     NUM_LABELS = 6
     DROPOUT = 0.1
     MAX_LENGTH = 512
