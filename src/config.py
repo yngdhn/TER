233a53b699326eda9863eefd232ccd4856030fe7
yngdhn <166201088+yngdhn@users.noreply.github.com>
2024-12-11 17:50:52 +0900

    Update config.py
    removed unnecessary lines

--- src/config.py
+++ src/config.py
@@ -7,30 +7,6 @@ class Paths:
     
     TEST_DATA = "dataset/test_data.csv"
 
-    SINGLE_ROBERTA_100p = "models/SingleTransferRoBERTa_100p.pt"
-    SINGLE_ROBERTA_10p = "models/SingleTransferRoBERTa_10p.pt"
-    SINGLE_ROBERTA_1p = "models/SingleTransferRoBERTa_1p.pt"
-
-    SINGLE_BERT_100p = "models/SingleTransferBERT_100p.pt"
-    SINGLE_BERT_10p = "models/SingleTransferBERT_10p.pt"
-    SINGLE_BERT_1p = "models/SingleTransferBERT_1p.pt"
-
-    SINGLE_MLBERT_100p = "models/SingleTransferMLBERT_100p.pt"
-    SINGLE_MLBERT_10p = "models/SingleTransferMLBERT_10p.pt"
-    SINGLE_MLBERT_1p = "models/SingleTransferMLBERT_1p.pt"
-
-    DOUBLE_MLBERT_100p = "models/DoubleTransferMLBERT_100p.pt"
-    DOUBLE_MLBERT_10p = "models/DoubleTransferMLBERT_10p.pt"
-    DOUBLE_MLBERT_1p = "models/DoubleTransferMLBERT_1p.pt"
-
-    DISTIL_BERT_100p = "models/DistilBERT_100p.pt"
-    DISTIL_BERT_10p = "models/DistilBERT_10p.pt"
-    DISTIL_BERT_1p = "models/DistilBERT_1p.pt"
-
-    LARGE_BERT_100p = "models/LargeBERT_100p.pt"
-    LARGE_BERT_10p = "models/LargeBERT_10p.pt"
-    LARGE_BERT_1p = "models/LargeBERT_1p.pt"
-
 class LabelMap:
     EMOTIONS = {"sadness": 0, "joy": 1, "love": 2, "anger": 3, "fear": 4, "surprise": 5}
 
